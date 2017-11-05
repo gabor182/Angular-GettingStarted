2cbd5ec0cebe8782a02169a85d8d1cd24d70ca6c
Gabor Schmied <schmied.gabor@gmail.com>
2017-11-05 14:19:31 +0100

    navigating to ProductDetailComponent from ProductListComponent

--- AcmeApplication/src/app/products/product-detail.component.ts
+++ AcmeApplication/src/app/products/product-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
+
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -11,9 +14,23 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute, private _router: Router, private _service: ProductService) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this._service.getProductById(id).subscribe(
+      data => {
+        this.product = data;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
--- AcmeApplication/src/app/products/product.service.ts
+++ AcmeApplication/src/app/products/product.service.ts
@@ -6,6 +6,7 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class ProductService {
@@ -20,6 +21,14 @@ export class ProductService {
         return this._http.get<IProduct[]>(this._productUrl)
                 .do(data => console.log('All: ' + JSON.stringify(data)))
                 .catch(this.handleError);
+    }
+
+    // Work in progress
+    getProductById(id: number): Observable<IProduct> {
+        return this._http.get<IProduct[]>(this._productUrl)
+        //.filter(products => products.find(product => product.productId == id) !== null)
+        .do(data => console.log('Product: ' + JSON.stringify(data)))
+        .catch(this.handleError);
     }
 
     private handleError(err: HttpErrorResponse) {
